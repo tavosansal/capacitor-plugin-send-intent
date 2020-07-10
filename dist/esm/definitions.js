36bed4309e529bc74b9fb8cfa560b3df90272516
Gustavo San Jose <gustavo.sanjose@coschedule.com>
2020-07-10 16:12:43 -0500

    [CHORE] Remove dist

--- dist/esm/definitions.js
+++
@@ -1,2 +0,0 @@
-;
-//# sourceMappingURL=definitions.js.map
--- dist/esm/web.d.ts
+++
@@ -1,7 +0,0 @@
-import { WebPlugin } from '@capacitor/core';
-import { SendIntentPlugin } from './definitions';
-export declare class SendIntentPluginWeb extends WebPlugin implements SendIntentPlugin {
-    constructor();
-}
-declare const SendIntent: SendIntentPluginWeb;
-export { SendIntent };
